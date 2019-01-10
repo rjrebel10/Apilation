5ee567379a1e0d31c9f1bffa3def838eaec2c9d9
rjrebel10 <31456118+rjrebel10@users.noreply.github.com>
2019-01-10 11:31:35 -0600

    Update Apilation.py

--- Apilation.py
+++ Apilation.py
@@ -1,7 +1,18 @@
+################################################################################################################################
+# Import Packages
 import requests
-
-def pipeline_output_collection(username,password,server,database,pipeline,source_collection,target_collection,append=True,
-                 gateway_port='8443',database_port='27117',host='localhost',subdatabase='lmrm__sonarg'):
+import urllib3
+import time
+################################################################################################################################
+# Turn off insecure request warnings that result from unverified ssl certificates
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+################################################################################################################################
+# Runs an Aggregation Pipeline on the specified source_collection in SonarG and outputs the results into the target_collection
+def pipeline_output_collection(username,password,server,database,pipeline,source_collection,target_collection,append=True,other_args=None,
+            print_info=True,gateway_port='8443',database_port='27117',host='localhost',subdatabase='lmrm__sonarg'):
+    true_false_vars=[append,print_info]
+    for var in true_false_vars:
+        assert var in [True,False]
     params={'username':username,
             'pwd':password,
             'db':database,
@@ -15,11 +26,26 @@ def pipeline_output_collection(username,password,server,database,pipeline,source
             'anyCert':'true'}
     if append is not True:
         params['dropTarget']='force'
+    if other_args is not None:
+        assert isinstance(other_args,dict)
+        for key,val in other_args.items():
+            params[key]=val
     url=concat_url(server,gateway_port)
-    return requests.get(url=url,params=params,timeout=1,verify=False)
-
-def pipeline_output_json(username,password,server,database,pipeline,source_collection,limit=None,
-                 gateway_port='8443',database_port='27117',host='localhost',subdatabase='lmrm__sonarg'):
+    if print_info:
+        print("Running Pipeline: "+pipeline)
+        start=time.time()
+    response = requests.get(url=url,params=params,timeout=1,verify=False)
+    if print_info:
+        print("  Execution Time: "+"{:.2f}s".format(time.time()-start))
+        print("  Response: "+response.text)
+    return response
+################################################################################################################################
+# Runs an Aggregation Pipeline on the specified source_collection in SonarG and provides the results as a json dictionary
+def pipeline_output_json(username,password,server,database,pipeline,source_collection,limit=None,other_args=None,
+            print_info=True,gateway_port='8443',database_port='27117',host='localhost',subdatabase='lmrm__sonarg'):
+    true_false_vars=[print_info]
+    for var in true_false_vars:
+        assert var in [True,False]
     params={'username':username,
             'pwd':password,
             'db':database,
@@ -31,12 +57,27 @@ def pipeline_output_json(username,password,server,database,pipeline,source_colle
             'sdb':subdatabase,
             'anyCert':'true'}
     if limit is not None:
-        params['limit']=limit
+        params['limit']=str(limit)
+    if other_args is not None:
+        assert isinstance(other_args,dict)
+        for key,val in other_args.items():
+            params[key]=val
     url=concat_url(server,gateway_port)
-    return requests.get(url=url,params=params,timeout=1,verify=False)
-
-def insert_json_array(username,password,server,database,target_collection,json_array,
-                 gateway_port='8443',database_port='27117',host='localhost',subdatabase='lmrm__sonarg'):
+    if print_info:
+        print("Running Pipeline: "+pipeline)
+        start=time.time()
+    response = requests.get(url=url,params=params,timeout=1,verify=False).json()
+    if print_info:
+        print("  Execution Time: "+"{:.2f}s".format(time.time()-start))
+        print("  Found: "+str(len(response))+" Records")
+    return response
+################################################################################################################################
+# Runs an Aggregation Pipeline on the specified source_collection in SonarG and provides the results as a json dictionary
+def insert_json_array(username,password,server,database,target_collection,json_array,other_args=None,
+            print_info=True,gateway_port='8443',database_port='27117',host='localhost',subdatabase='lmrm__sonarg'):
+    true_false_vars=[print_info]
+    for var in true_false_vars:
+        assert var in [True,False]
     params={'username':username,
             'pwd':password,
             'db':database,
@@ -48,8 +89,21 @@ def insert_json_array(username,password,server,database,target_collection,json_a
             'port':str(database_port),
             'sdb':subdatabase,
             'anyCert':'true'}
+    if other_args is not None:
+        assert isinstance(other_args,dict)
+        for key,val in other_args.items():
+            params[key]=val
     url=concat_url(server,gateway_port)
-    return requests.get(url=url,params=params,timeout=1,verify=False)
-
+    if print_info:
+        print("Inserting Data into: "+target_collection)
+        start=time.time()
+    response = requests.get(url=url,params=params,timeout=1,verify=False)
+    if print_info:
+        print("  Execution Time: "+"{:.2f}s".format(time.time()-start))
+        print("  Response: "+response.text)
+    return response
+################################################################################################################################
+# Structures the server url with its connection type and endpoint
 def concat_url(server,port,con_type='https://',endpoint='Gateway'):
     return con_type+str(server)+':'+str(port)+'/'+endpoint
+################################################################################################################################
